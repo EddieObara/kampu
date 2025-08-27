b0cbad6e0720a06160c03c99067e86b0b9598d33
EddieObara <eddiebello20@gmail.com>
2025-08-27 20:26:02 +0300

    one last thing

--- contact.php
+++ contact.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// If browser sends a preflight OPTIONS request, just exit
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
